e14eabf2fe2c04ff0babd0129e96c27b72e09164
mbax <matt@phozop.net>
2014-02-15 15:35:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+CraftIRC
+========
+
+This repository is for CraftIRC 4, a freshly built evolution of CraftIRC.
+
+It will contain no code from previous iterations, and will be released under a more flexible license.
---
+++ src/main/java/org/kitteh/craftirc/CraftIRC.java
@@ -0,0 +1,10 @@
+package org.kitteh.craftirc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftIRC extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        this.getLogger().info("I do nothing!");
+    }
+}
