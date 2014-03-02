4ec26fbc5ca6e28dc6541b9c759f56532c06245e
mbax <matt@phozop.net>
2014-03-02 17:35:46 -0500

    Just for @dequis

---
+++ src/main/java/org/kitteh/craftirc/endpoint/filter/defaults/RegexFilter.java
@@ -0,0 +1,14 @@
+package org.kitteh.craftirc.endpoint.filter.defaults;
+
+import org.kitteh.craftirc.endpoint.filter.Filter;
+import org.kitteh.craftirc.message.EndpointMessage;
+
+/**
+ * Filter of information into the displayed message, via regular expression.
+ */
+public class RegexFilter implements Filter {
+    @Override
+    public void processIncomingMessage(EndpointMessage message) {
+        // TODO get dx to do this
+    }
+}
