b6e9d162979e8233137aecc6adab48d0798b74ef
mbax <matt@phozop.net>
2014-03-02 22:18:59 -0500

    Light scrubbing

--- src/main/java/org/kitteh/craftirc/exceptions/CraftIRCFoundTabsException.java
+++ src/main/java/org/kitteh/craftirc/exceptions/CraftIRCFoundTabsException.java
@@ -3,7 +3,7 @@ package org.kitteh.craftirc.exceptions;
 /**
  * OH NO, TABS
  */
-public class CraftIRCFoundTabsException extends CraftIRCInvalidConfigException {
+public final class CraftIRCFoundTabsException extends CraftIRCInvalidConfigException {
     public CraftIRCFoundTabsException(int lineNumber, String line) {
         super("Config cannot contain tabs. Found a tab on line " + lineNumber + ": " + line.replace("\t", ""));
     }
--- src/main/java/org/kitteh/craftirc/util/MinecraftPlayer.java
+++ src/main/java/org/kitteh/craftirc/util/MinecraftPlayer.java
@@ -1,24 +1,53 @@
 package org.kitteh.craftirc.util;
 
+import org.apache.commons.lang.Validate;
+
 import java.util.UUID;
 
 /**
  * Represents an ingame player
  */
 public final class MinecraftPlayer {
-    private UUID uniqueId;
     private String name;
+    private UUID uniqueId;
 
+    /**
+     * Creates a MinecraftPlayer instance.
+     *
+     * @param name current name of the player
+     * @param uniqueId the player's UUID
+     */
     public MinecraftPlayer(String name, UUID uniqueId) {
+        Validate.notNull(name, "Name cannot be null");
+        Validate.notNull(uniqueId, "uniqueId cannot be null");
         this.name = name;
         this.uniqueId = uniqueId;
     }
 
+    /**
+     * Gets the name of this player.
+     *
+     * @return the player's name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the UUID of this player.
+     *
+     * @return the player's UUID
+     */
     public UUID getUniqueID() {
         return this.uniqueId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof MinecraftPlayer) {
+            MinecraftPlayer p = (MinecraftPlayer) o;
+            return this.name.equals(p.name) && this.uniqueId.equals(p.uniqueId);
+        }
+        return false;
     }
 }
