27be293d463ea1d95dc78d4ebb5d40d75077b809
mbax <matt@phozop.net>
2017-07-25 20:23:27 -0400

    It's 2017.

--- src/main/java/org/kitteh/craftirc/exceptions/CraftIRCFoundTabsException.java
+++ src/main/java/org/kitteh/craftirc/exceptions/CraftIRCFoundTabsException.java
@@ -1,5 +1,5 @@
 /*
- * * Copyright (C) 2014-2016 Matt Baxter http://kitteh.org
+ * * Copyright (C) 2014-2017 Matt Baxter http://kitteh.org
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
